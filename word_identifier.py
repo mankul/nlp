82ac50fd34e58717d6460c62a95a99834544d5ea
mankul <mankulgupta@gmail.com>
2018-01-10 11:24:55 +0530

    all local files are pushed

---
+++ word_identifier.py
@@ -0,0 +1,33 @@
+
+
+
+
+
+def main():
+	string1=raw_input()	
+	#load_nouns()
+	lexical_analyzer(string1)
+def lexical_analyzer(string1):
+	print string1
+	word=""
+	list_of_noun=open('nounlist_1.txt','r')
+	list_of_noun=list_of_noun.read()
+	print list_of_noun
+	list_noun=[]
+	for character in list_of_noun:
+		if character == "\n" and word != "":
+			list_noun.append(word)
+			word=""
+		else:
+			word+=character
+	word=""	
+	list_noun=sorted(list_noun)
+	for character in string1:
+		if character==" ":
+			for character in word:
+				
+			word=""
+		else:
+			word+=character	
+
+if __name__ == "__main__":main()
